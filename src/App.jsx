b4954c9e6d00fba208f028e39d362462ee23c10a
farguello <leporcmusic@gmail.com>
2022-04-15 23:30:02 -0300

    checkpoint commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import PokemonCard from './components/PokemonCard';
 
 function App() {
+  const [pokemon, setPokemon] = useState('pikachu');
+  const [pokemonSprite, setPokemonSprite] = useState('');
 
-  return (
-    <div className="App">
-      <p>HI!</p>
-    </div>
-  )
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+
+    useEffect(() => {
+      getPokemon();
+    }, []);
+
+    return (
+      <div id="main-view" className="">
+        <PokemonCard className="" />
+      </div>
+    )
+  }
 }
 
 export default App
